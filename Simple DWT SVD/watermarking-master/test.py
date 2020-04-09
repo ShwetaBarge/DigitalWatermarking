3a55b9c58a879e0f4798e067036b35db9ad8cbd4
ShwetaBarge <bargeshweta983@gmail.com>
2020-04-09 19:25:46 +0530

    Deleted unnecessary files

--- Simple DWT SVD/watermarking-master/test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2018/5/30
-# @Author  : Luke
-
-from watermarking import watermarking
-
-watermarking = watermarking()
-watermarking.watermark()
-watermarking.extracted()
